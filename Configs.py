b627a7b25500026d58b04db2aba25a83ff50e2c7
Bazibro <96934642+hyko-xd@users.noreply.github.com>
2022-01-25 22:18:53 +0530

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -8,3 +8,33 @@ class Config(object):
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
     RULES = os.environ.get("RULES", None)
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "BAZIGARXD")
+
+    
+def get_list_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.list(name, default=default)) and not required:
+        log.warn("No list key: " + name)
+        return []
+    elif not data:
+        log.critical("No list key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_bool_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.bool(name, default=default)) and not required:
+        log.warn("No bool key: " + name)
+        return False
+    elif not data:
+        log.critical("No bool key: " + name)
+        sys.exit(2)
+    else:
+        return data
